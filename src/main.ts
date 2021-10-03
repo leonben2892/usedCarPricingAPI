eb33b4711b67967e76c19c433809a7d273dcb87c
Leon Benjamin <LeonB@shufersal.co.il>
2021-10-03 13:31:08 +0300

    added authentication functionality

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,25 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(
+    cookieSession({
+      keys: ['asdfasdf'],
+    }),
+  );
+
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true
-    })
+      whitelist: true,
+    }),
   );
+
   app.setGlobalPrefix('api');
+
   await app.listen(3000);
 }
 bootstrap();
