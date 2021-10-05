2ee9e808cbf85d49fd01a8f15b2514fe02a5d942
Leon Benjamin <LeonB@shufersal.co.il>
2021-10-05 08:01:09 +0300

    added e2e tests

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -16,7 +16,7 @@ describe('Authentication System', () => {
   });
 
   it('handles a signup request', () => {
-    const email = 'e2e2@e2e.com';
+    const email = 'e2e@e2e.com';
     return request(app.getHttpServer())
       .post('/auth/signup')
       .send({ email, password: '1234' })
@@ -26,5 +26,23 @@ describe('Authentication System', () => {
         expect(id).toBeDefined();
         expect(email).toEqual(email);
       });
+  });
+
+  it('signup as a new user then get the currently logged in user', async () => {
+    const email = 'asdf@asdf.com';
+
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: '1234' })
+      .expect(201);
+
+    const cookie = res.get('Set-Cookie');
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200);
+
+    expect(body.email).toEqual(email);
   });
 });
